2fb0df9e43069063d15a7d0cec57d051d6e2d337
Daniel_Kim <60537388+kikiru328@users.noreply.github.com>
2025-06-18 07:51:16 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 5. 객체지향 2, Time: 59 ms, Memory: 50,944 KB -BaekjoonHub

---
+++ SWEA/D1/6208. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 2/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 2.py
@@ -0,0 +1,11 @@
+class Korean:
+    
+    def __init__(self):
+        self.printNationality()
+    
+    @staticmethod
+    def printNationality():
+        print("대한민국")
+        
+nation = Korean()
+nation.printNationality()
